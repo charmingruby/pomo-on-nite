4eb44a445a9fc6a161c54150cd92fdc526ba215c
charmingruby <mrrubycanal3105@gmail.com>
2023-06-03 12:19:14 -0300

    pages and routes

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,5 +1,56 @@
-import { Title } from './styles'
+import {
+  FormContainer,
+  HomeContainer,
+  CountdownContainer,
+  Separator,
+  StartCountdownButton,
+  TaskInput,
+  MinutesAmountInput,
+} from './styles'
+import { Play } from 'phosphor-react'
 
 export function Home() {
-  return <Title>Home</Title>
+  return (
+    <HomeContainer>
+      <form action="">
+        <FormContainer>
+          <label htmlFor="task">I{"'"}ll working in</label>
+          <TaskInput
+            id="task"
+            list="task-suggestions"
+            placeholder="Give a name for your project"
+          />
+
+          <datalist id="task-suggestions">
+            <option value="Project 1" />
+          </datalist>
+
+          <label htmlFor="minutesAmount">during</label>
+          <MinutesAmountInput
+            type="number"
+            id="minutesAmount"
+            placeholder="00"
+            step={5}
+            min={5}
+            max={60}
+          />
+
+          <span>minutes.</span>
+        </FormContainer>
+
+        <CountdownContainer>
+          <span>0</span>
+          <span>0</span>
+          <Separator>:</Separator>
+          <span>0</span>
+          <span>0</span>
+        </CountdownContainer>
+
+        <StartCountdownButton disabled type="submit">
+          <Play size={24} />
+          Start
+        </StartCountdownButton>
+      </form>
+    </HomeContainer>
+  )
 }
